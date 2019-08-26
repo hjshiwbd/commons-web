ca6ec53c8e5a48714ecf3d9389f6a3bb577db923
huangjin <hjin@hb-rz.com>
2019-08-26 11:02:06 +0800

    dev

--- src/main/java/hjin/commons/web/spring/CommonControllerAdvisor.java
+++ src/main/java/hjin/commons/web/spring/CommonControllerAdvisor.java
@@ -4,10 +4,8 @@ import hjin.commons.web.session.ISessionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
 public class CommonControllerAdvisor {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
